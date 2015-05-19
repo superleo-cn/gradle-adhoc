160b7e0b0864695db9736606f33766ff554783b5
superleo <superleo.peking@gmail.com>
2015-05-19 22:58:21 +0800

    Add new files.

--- src/com/leo/MyUtil.java
+++ src/com/leo/MyUtil.java
@@ -12,5 +12,6 @@ public class MyUtil {
 
     public static void main(String [] args){
         System.out.println(MyUtil.class.getResource("/aa.xml").getPath());
+        System.out.println("===");
     }
 }
